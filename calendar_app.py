7d9a15251aa6a94e229a7f1d8444e7b4080ab78e
Saket-Rama <163721169+Saket-Rama@users.noreply.github.com>
2026-08-28 12:36:41 +0530

    specifically created variables for mm and yr

--- calendar_app.py
+++ calendar_app.py
@@ -1,11 +1,12 @@
 import calendar
 choice = int(input("1.View Month\n2.View Entire Year\n3.Exit\nEnter the Option you want to choose:\n"))
-mm = int(input("Enter the Month:\n"))
-yr = int(input("Enter the Year:\n"))
 match choice:
     case 1:
+        mm = int(input("Enter the Month:\n"))
+        yr = int(input("Enter the Year:\n"))
         print(calendar.month(yr,mm))
     case 2:
+        yr = int(input("Enter the Year:\n"))
         print(calendar.calendar(yr))
     case _:
         print("Exit from the app!")
