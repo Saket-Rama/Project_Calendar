7f0dfd0a2d23041b579670ee43a89e89149f1367
Saket-Rama <163721169+Saket-Rama@users.noreply.github.com>
2026-08-28 12:35:16 +0530

    Printed the year also

--- calendar_app.py
+++ calendar_app.py
@@ -1,7 +1,11 @@
 import calendar
 choice = int(input("1.View Month\n2.View Entire Year\n3.Exit\nEnter the Option you want to choose:\n"))
-yr = int(input("Enter the Year:\n"))
 mm = int(input("Enter the Month:\n"))
+yr = int(input("Enter the Year:\n"))
 match choice:
     case 1:
         print(calendar.month(yr,mm))
+    case 2:
+        print(calendar.calendar(yr))
+    case _:
+        print("Exit from the app!")
